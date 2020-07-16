0e4eb805252cee2c5a3fc57cc279019edd9e9f92
Daniel Zhu <danielfangzhu@gmail.com>
2020-07-16 15:11:05 -0700

    First commit

---
+++ Assets/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isRunning", true);
+            transform.Translate(0, 0, 100f * Time.deltaTime);
+        }
+        else anim.SetBool("isRunning", false);
+        transform.Rotate(0, 20f * Time.deltaTime * Input.GetAxis("Horizontal"), 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("isJumping");
+        }
+    }
+}
